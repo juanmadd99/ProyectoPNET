708f72688f21fafee3361e4aa947396a1e1cf781
jonasnow <jonasnunezdiaz@gmail.com>
2023-03-24 12:40:38 +0100

    Botones oscuros corregidos

--- cod/js/accesibilidad.js
+++ cod/js/accesibilidad.js
@@ -42,3 +42,44 @@ function luna(){
     iconoSol.style.display = "block";
 
 }
+
+function solSinAside(){
+    let section = document.getElementsByTagName("section")
+    let nav = document.getElementsByTagName("nav")
+    let header = document.getElementsByTagName("header")
+    let footer = document.getElementsByTagName("footer")
+    let iconoSol = document.getElementById("sol")
+    let iconoLuna = document.getElementById("luna")
+    let fondo = document.getElementsByTagName("html")
+
+    section[0].style.background = "#3B7520"
+    nav[0].style.background = "#3B7520"
+    header[0].style.background = "#3B7520"
+    footer[0].style.background = "#3B7520"
+    fondo[0].style.background = "white"
+
+
+    iconoSol.style.display = "none";
+    iconoLuna.style.display = "block";
+
+}
+
+function lunaSinAside(){
+    let section = document.getElementsByTagName("section")
+    let nav = document.getElementsByTagName("nav")
+    let header = document.getElementsByTagName("header")
+    let footer = document.getElementsByTagName("footer")
+    let iconoSol = document.getElementById("sol")
+    let iconoLuna = document.getElementById("luna")
+    let fondo = document.getElementsByTagName("html")
+
+    section[0].style.background = "#3C4742"
+    nav[0].style.background = "#3C4742"
+    header[0].style.background = "#3C4742"
+    footer[0].style.background = "#3C4742"
+    fondo[0].style.background = "#000000"
+
+    iconoLuna.style.display = "none";
+    iconoSol.style.display = "block";
+
+}
